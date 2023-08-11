924683524b99588cc45220f52e9efc791e6923ad
Alejandro Quiñones <alejandro.quinones@outlook.es>
2023-08-10 20:10:57 -0400

    usuario

---
+++ src/main/java/com/example/modulo5/model/Usuario.java
@@ -0,0 +1,27 @@
+package com.example.modulo5.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Usuario {
+    private int id;
+    private int rut;
+    private String nombres;
+    private String apellido1;
+    private String apellido2;
+    private String correo_electronico;
+    private int telefono;
+    private String direccion;
+    private int id_tipo_usuario;
+    private String usuario_ingreso;
+    private String password;
+    private int id_cliente;
+
+
+}
