2430f9bd9926a94df736e43f510dde215faa00aa
Pablo Aliaga <pandresaliaga@gmail.com>
2023-08-10 20:42:22 -0400

    create accidente class

---
+++ src/main/java/com/example/modulo5/model/Accidente.java
@@ -0,0 +1,23 @@
+package com.example.modulo5.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public  class Accidente {
+ private int id;
+ private LocalDate fechaAccidente;
+ private LocalTime horaAccidente;
+ private int idTrabajador;
+ private String lugarAccidente;
+ private String descripcionAccidente;
+ private String tipoAccidente;
+ private int idCliente;
+
+}
