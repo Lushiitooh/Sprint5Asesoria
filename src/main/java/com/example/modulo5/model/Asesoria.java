9af4eba459ad55e374ae7a3f0a9f943a6fde4ac8
fvmolina21 <fvmolina@uc.cl>
2023-08-10 20:31:52 -0400

    Se agrega lombok

--- src/main/java/com/example/modulo5/model/Asesoria.java
+++ src/main/java/com/example/modulo5/model/Asesoria.java
@@ -1,7 +1,9 @@
 package com.example.modulo5.model;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import lombok.Data;
 
+@Data
 public class Asesoria {
     private int id;
     private String nombre_asesoria;
@@ -10,74 +12,4 @@ public class Asesoria {
     private LocalDate fecha_ingreso;
     private int id_cliente_peticion;
     private int estado;
-
-    // Constructor
-    public Asesoria(int id, String nombre_asesoria, int id_tipo_riesgo, String descripcion_asesoria,
-                    LocalDate fecha_ingreso, int id_cliente_peticion, int estado) {
-        this.id = id;
-        this.nombre_asesoria = nombre_asesoria;
-        this.id_tipo_riesgo = id_tipo_riesgo;
-        this.descripcion_asesoria = descripcion_asesoria;
-        this.fecha_ingreso = fecha_ingreso;
-        this.id_cliente_peticion = id_cliente_peticion;
-        this.estado = estado;
-    }
-
-    // Getters
-    public int getId() {
-        return id;
-    }
-
-    public String getNombre_asesoria() {
-        return nombre_asesoria;
-    }
-
-    public int getId_tipo_riesgo() {
-        return id_tipo_riesgo;
-    }
-
-    public String getDescripcion_asesoria() {
-        return descripcion_asesoria;
-    }
-
-    public LocalDate getFecha_ingreso() {
-        return fecha_ingreso;
-    }
-
-    public int getId_cliente_peticion() {
-        return id_cliente_peticion;
-    }
-
-    public int getEstado() {
-        return estado;
-    }
-
-    // Setters
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setNombre_asesoria(String nombre_asesoria) {
-        this.nombre_asesoria = nombre_asesoria;
-    }
-
-    public void setId_tipo_riesgo(int id_tipo_riesgo) {
-        this.id_tipo_riesgo = id_tipo_riesgo;
-    }
-
-    public void setDescripcion_asesoria(String descripcion_asesoria) {
-        this.descripcion_asesoria = descripcion_asesoria;
-    }
-
-    public void setFecha_ingreso(LocalDate fecha_ingreso) {
-        this.fecha_ingreso = fecha_ingreso;
-    }
-
-    public void setId_cliente_peticion(int id_cliente_peticion) {
-        this.id_cliente_peticion = id_cliente_peticion;
-    }
-
-    public void setEstado(int estado) {
-        this.estado = estado;
-    }
 }
--- src/main/java/com/example/modulo5/model/Tarea.java
+++ src/main/java/com/example/modulo5/model/Tarea.java
@@ -1,6 +1,8 @@
 package com.example.modulo5.model;
 import java.time.LocalDate;
+import lombok.Data;
 
+@Data
 public class Tarea {
     private int id;
     private String nombre_tarea;
@@ -11,93 +13,4 @@ public class Tarea {
     private int id_estado;
     private int id_tipo_tarea;
     private int id_asesoria;
-
-    // Constructor
-    public Tarea(int id, String nombre_tarea, String descripcion, String entregable,
-                 LocalDate fecha_ingreso, LocalDate fecha_ejecucion, int id_estado,
-                 int id_tipo_tarea, int id_asesoria) {
-        this.id = id;
-        this.nombre_tarea = nombre_tarea;
-        this.descripcion = descripcion;
-        this.entregable = entregable;
-        this.fecha_ingreso = fecha_ingreso;
-        this.fecha_ejecucion = fecha_ejecucion;
-        this.id_estado = id_estado;
-        this.id_tipo_tarea = id_tipo_tarea;
-        this.id_asesoria = id_asesoria;
-    }
-
-    // Getters
-    public int getId() {
-        return id;
-    }
-
-    public String getNombre_tarea() {
-        return nombre_tarea;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public String getEntregable() {
-        return entregable;
-    }
-
-    public LocalDate getFecha_ingreso() {
-        return fecha_ingreso;
-    }
-
-    public LocalDate getFecha_ejecucion() {
-        return fecha_ejecucion;
-    }
-
-    public int getId_estado() {
-        return id_estado;
-    }
-
-    public int getId_tipo_tarea() {
-        return id_tipo_tarea;
-    }
-
-    public int getId_asesoria() {
-        return id_asesoria;
-    }
-
-    // Setters
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setNombre_tarea(String nombre_tarea) {
-        this.nombre_tarea = nombre_tarea;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public void setEntregable(String entregable) {
-        this.entregable = entregable;
-    }
-
-    public void setFecha_ingreso(LocalDate fecha_ingreso) {
-        this.fecha_ingreso = fecha_ingreso;
-    }
-
-    public void setFecha_ejecucion(LocalDate fecha_ejecucion) {
-        this.fecha_ejecucion = fecha_ejecucion;
-    }
-
-    public void setId_estado(int id_estado) {
-        this.id_estado = id_estado;
-    }
-
-    public void setId_tipo_tarea(int id_tipo_tarea) {
-        this.id_tipo_tarea = id_tipo_tarea;
-    }
-
-    public void setId_asesoria(int id_asesoria) {
-        this.id_asesoria = id_asesoria;
-    }
 }
