18975368df2848e92f28923af3bd6886ddb720ca
Gabrielcorderomutinelli@gmail.com <gabrielcorderomutinelli@gmail-com>
2023-08-10 20:18:50 -0400

    Agregando clase Cliente

---
+++ src/main/java/com/example/modulo5/model/Cliente.java
@@ -0,0 +1,28 @@
+package com.example.modulo5.model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Cliente extends Usuario{
+
+    private int id;
+    private int rut;
+    private String razon_social;
+    private String nombre_contacto;
+    private String direccion;
+    private int telefono;
+    private String correo_electronico;
+    private LocalDate fechaingreso = new LocalDate ();
+
+
+
+}
