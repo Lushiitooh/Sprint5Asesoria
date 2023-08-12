b726b9c8d12d7eb4a66110d9dbdec6063f683e2c
x9laaa <acarreno95@outlook.com>
2023-08-11 20:46:23 -0400

    crud de interface tarea

--- src/main/java/com/example/modulo5/dao/ITarea.java
+++ src/main/java/com/example/modulo5/dao/ITarea.java
@@ -1,5 +1,13 @@
 package com.example.modulo5.dao;
 
+import com.example.modulo5.model.Tarea;
+
+import java.util.List;
+
 public interface ITarea {
+    public Tarea crearTarea(Tarea tarea);
+    public List<Tarea> listarTarea();
+    public Tarea actualizarTarea(Tarea tarea);
+    public  boolean eliminarTarea(int idTarea);
 
 }
