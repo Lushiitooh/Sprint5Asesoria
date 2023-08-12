626d10df3856e59d4e45884c93bb78571dec05e0
cpmeneses <cpmeneses94@gmail.com>
2023-08-11 21:59:59 -0400

    dao Interfaz IAsesoria

--- src/main/java/com/example/modulo5/dao/IAsesoria.java
+++ src/main/java/com/example/modulo5/dao/IAsesoria.java
@@ -1,9 +1,13 @@
 package com.example.modulo5.dao;
 
-import com.example.modulo5.model.Tarea;
+import com.example.modulo5.model.Asesoria;
 
 import java.util.List;
 
-public interface ITarea {
-
+public interface IAsesoria {
+    public Asesoria crearAsesoria(Asesoria asesoria);
+    public List<Asesoria> listarAsesoria();
+    public List<Asesoria> listarAsesoriaCliente(int idCliente);
+    public Asesoria actualizarAsesoria(Asesoria asesoria);
+    public boolean eliminarAsesoria(int idAsesoria);
 }
