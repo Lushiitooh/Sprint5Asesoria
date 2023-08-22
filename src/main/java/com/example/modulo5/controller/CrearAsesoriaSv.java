30a2a8a513be8aa06e63268fdd13ccc6186fb0fe
cpmeneses <cpmeneses94@gmail.com>
2023-08-21 20:44:33 -0400

    crear asesorias servlet falta poco

--- src/main/java/com/example/modulo5/controller/CrearAsesoriaSv.java
+++ src/main/java/com/example/modulo5/controller/CrearAsesoriaSv.java
@@ -3,6 +3,7 @@ package com.example.modulo5.controller;
 import com.example.modulo5.dao.IAsesoria;
 import com.example.modulo5.dao.daoimpl.AsesoriaDaoImpl;
 import com.example.modulo5.model.Asesoria;
+import com.example.modulo5.model.Trabajador;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @WebServlet("/crearAsesoriasSv")
@@ -34,9 +36,18 @@ public class CrearAsesoriaSv extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //IAsesoria asesoriaDaoImpl = new AsesoriaDaoImpl();
-        //Asesoria asesoria =
-        //request.setAttribute("asesorias", asesorias);
-        //request.getRequestDispatcher("views/listarAsesorias.jsp").forward(request, response);
+        IAsesoria asesoriaDaoImpl = new AsesoriaDaoImpl();
+        Asesoria asesoria = new Asesoria(
+                0,
+                request.getParameter("nombre_asesoria"),
+                Integer.parseInt(request.getParameter("id_tipo_riesgo")),
+                request.getParameter("descripcion_asesoria"),
+                LocalDateTime.now(),
+                0,
+                true
+
+        );
+        asesoriaDaoImpl.crearAsesoria(asesoria);
+        //response.sendRedirect(request.getContextPath() + "/listarAsesorias.jsp");
     }
 }
