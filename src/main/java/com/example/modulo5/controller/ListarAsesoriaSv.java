ceb3c4c4b716c7dc9b201688240ff205628b099e
cpmeneses <cpmeneses94@gmail.com>
2023-08-21 21:16:34 -0400

    Servlets de asesoria

---
+++ src/main/java/com/example/modulo5/controller/ListarAsesoriaSv.java
@@ -0,0 +1,26 @@
+package com.example.modulo5.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/ListarAsesoriasSv")
+public class ListarAsesoriaSv extends HttpServlet {
+    public ListarAsesoriaSv() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
