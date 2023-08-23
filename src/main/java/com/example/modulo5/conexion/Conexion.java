c5b3658358e124f8913b41391643cf976c0bdb82
Lushiitooh <luis.sepu.c@gmail.com>
2023-08-22 22:42:59 -0400

    hola

--- src/main/java/com/example/modulo5/conexion/Conexion.java
+++ src/main/java/com/example/modulo5/conexion/Conexion.java
@@ -5,54 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-<<<<<<< HEAD
-   private static Connection conexion = null;
-   private Conexion() {
-       try {
-           Class.forName("com.mysql.cj.jdbc.Driver");
-           conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/sgas_bd", "root", "VamosConTodo1234&");
-
-       } catch (ClassNotFoundException exception) {
-           exception.printStackTrace();
-       } catch (SQLException e) {
-           e.printStackTrace();
-       }
-   }
-    public static Connection getConexion(){
-       if(conexion == null){
-           new Conexion();
-       }
-       return conexion;
-
-=======
     private static Connection conexion = null;
->>>>>>> mechavaa
-
-    // Constructor privado para asegurarse de que sólo se pueda acceder desde dentro de la clase
     private Conexion() {
         try {
-            // Cargando el driver de MySQL
             Class.forName("com.mysql.cj.jdbc.Driver");
-            // Estableciendo la conexión
-            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/sgas_bd", "java", "admin");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/sgas_bd", "root", "admin");
+
         } catch (ClassNotFoundException exception) {
-            exception.printStackTrace();  // Esto se podría manejar mejor con un logger
+            exception.printStackTrace();
         } catch (SQLException e) {
-            e.printStackTrace();  // Esto se podría manejar mejor con un logger
+            e.printStackTrace();
         }
     }
-
-    // Método público estático para obtener la conexión
-    public static Connection getConexion() {
-        try {
-            // Si la conexión es nula o está cerrada, crea una nueva conexión
-            if (conexion == null || conexion.isClosed()) {
-                new Conexion();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();  // Esto se podría manejar mejor con un logger
+    public static Connection getConexion(){
+        if(conexion == null){
+            new Conexion();
         }
         return conexion;
+
+
     }
 }
-
