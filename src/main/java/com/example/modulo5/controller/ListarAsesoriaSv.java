f45c62cf34d151b5f617c7498f4c92f05b15bc76
cpmeneses <cpmeneses94@gmail.com>
2023-08-22 21:45:26 -0400

    ListarAsesoriaSv

--- src/main/java/com/example/modulo5/controller/ListarAsesoriaSv.java
+++ src/main/java/com/example/modulo5/controller/ListarAsesoriaSv.java
@@ -1,5 +1,7 @@
 package com.example.modulo5.controller;
 
+import com.example.modulo5.dao.daoimpl.AsesoriaDaoImpl;
+import com.example.modulo5.model.Asesoria;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,20 +9,33 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet("/ListarAsesoriasSv")
+@WebServlet("/ListarAsesoriaSv")
 public class ListarAsesoriaSv extends HttpServlet {
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
     public ListarAsesoriaSv() {
         super();
     }
 
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Served at: ").append(request.getContextPath());
     }
 
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        AsesoriaDaoImpl asesoriaDaoImpl = new AsesoriaDaoImpl();
+        List<Asesoria> asesorias = asesoriaDaoImpl.listarAsesoria();
+        request.setAttribute("asesorias", asesorias);
+        request.getRequestDispatcher("views/listarAsesorias.jsp").forward(request, response);
     }
 }
